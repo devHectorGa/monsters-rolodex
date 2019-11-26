8eae3a5960e24a8bfb6720bf32c69dad0fc9ab8a
devHectorGa <Hetikor@outlook.com>
2019-11-25 19:47:37 -0500

    Updates

---
+++ precache-manifest.6d75ad6c3b6eae7aa13fa6d2d07eb275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e787024de366e2d88c7fd2bf06cb1a8e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e78e3be202a5bf2687b2",
+    "url": "/monsters-rolodex/static/css/main.d4e0a591.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "e78e3be202a5bf2687b2",
+    "url": "/monsters-rolodex/static/js/main.7680013a.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
